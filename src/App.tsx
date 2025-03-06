6148c64fa6a99e1e861daa863fb432a5227b09c9
Roberto Pinales <roberto@mac.lan>
2025-03-06 15:30:29 -0500

    Initial commit- Add LoginScreen and dependecies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginScreen from './components/login-screen';
+
+function App() {
+	return (
+		<div className="App">
+			<LoginScreen />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/login-screen.tsx
@@ -0,0 +1,16 @@
+import React, { JSX } from 'react';
+
+interface LoginScreenProps {
+};
+
+function LoginScreen(props: LoginScreenProps): JSX.Element {
+    const {
+
+    } = props;
+
+    return (
+        <p>{"Login"}</p>
+    );
+};
+
+export default LoginScreen;
